333c10c4f1aa71db39413b937d29a4e95dbb487f
imoranmo <imoranmo@gmail.com>
2021-10-14 18:33:29 -0700

    router updates

--- routes/api.js
+++ routes/api.js
@@ -1,1 +1,17 @@
 const router = require("express").Router();
+const Workouts = require("../models/workouts");
+const path = require("path");
+
+app.get("/", function (req, res){
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.get("/exercise", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+app.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+module.exports = router;
